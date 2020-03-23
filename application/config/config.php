9b38aee5f5ab093adc7247b9ecb2108610b90581
Dimas Awang Kusuma <arjunane.kajen@gmail.com>
2020-03-23 12:34:04 +0700

    update base_url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/mencoba-chat2/';
+$config['base_url'] = 'http://localhost/simple-chat-2/';
 
 /*
 |--------------------------------------------------------------------------
